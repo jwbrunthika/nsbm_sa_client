676e342b97ddd53b4ad92f4ae9987d1be84887a0
Ometh Abeyrathne <ometha12@gmail.com>
2025-02-12 12:26:07 +0530

    minor adjustments

--- app/(main_screen)/index.tsx
+++ app/(main_screen)/index.tsx
@@ -177,6 +177,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    position:"absolute",
     backgroundColor: "#FFFFF",
   },
   header: {
